608cf143045c8d75b4475e1ca39480389459bed3
WORK <jacobkiller1235@proton.me>
2025-12-24 01:14:07 +0500

    правки

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\Api\TelegramController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BonusController;
+use App\Http\Controllers\ReferralController;
 use App\Models\Games;
 
 Route::get('/', function () {
@@ -29,5 +31,12 @@ Route::get('login', [AuthController::class, 'login'])->name('login');
 
 Route::get('tg/auth', [AuthController::class, 'telegramAuth'])->name('tg.auth');
 Route::post('tg/auth/login', [AuthController::class, 'telegramLogin'])->name('tg.auth.login');
+
+Route::middleware('auth')->group(function () {
+    Route::post('api/bonus/daily', [BonusController::class, 'bonusDaily'])->name('bonus.daily');
+    Route::post('api/bonus/promo', [BonusController::class, 'activatePromo'])->name('bonus.promo');
+    Route::post('api/bonus/check-subscriptions', [BonusController::class, 'checkSubscriptions'])->name('bonus.check-subscriptions');
+    Route::post('api/referral/claim', [ReferralController::class, 'claim'])->name('referral.claim');
+});
 
 require_once 'games.php';
